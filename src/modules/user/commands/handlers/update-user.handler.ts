78af5093f1cd24a457ce6866cf2ff3acd225c157
TMNhat1810 <minhnhat912002@gmail.com>
2024-10-11 10:54:05 +0700

    chores: config command to format prisma files

--- src/modules/user/commands/handlers/update-user.handler.ts
+++ src/modules/user/commands/handlers/update-user.handler.ts
@@ -3,6 +3,7 @@ import { UpdateUserCommand } from '../implements';
 import { UserRepository } from '../../repositories/user.repository';
 import { RoleRepository } from 'src/modules/role/repositories/role.repository';
 import { Role } from 'src/common/enums';
+import { NotFoundException } from '@nestjs/common';
 
 @CommandHandler(UpdateUserCommand)
 export class UpdateUserHandler implements ICommandHandler<UpdateUserCommand> {
@@ -15,6 +16,8 @@ export class UpdateUserHandler implements ICommandHandler<UpdateUserCommand> {
 		const { id, updateUserDto } = command;
 
 		const user = await this.userRepository.getById(id);
+		if (!user) return new NotFoundException();
+
 		const role = await this.roleRepository.getById(user.id_role);
 
 		if (role.name === Role.Lecturer)

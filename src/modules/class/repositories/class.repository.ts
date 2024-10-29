83350aefe33f4871e325654da06a5a3d5dd9df04
TMNhat1810 <minhnhat912002@gmail.com>
2024-10-29 09:20:42 +0700

    feat: add get all classes api

--- src/modules/class/repositories/class.repository.ts
+++ src/modules/class/repositories/class.repository.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/modules/database/services';
 
 @Injectable()
-export class ClassRepository {}
+export class ClassRepository {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async getAllClass(skip: number, take: number): Promise<[any[], number]> {
+		try {
+			const [classes, totalRecords] = await Promise.all([
+				this.prisma.class.findMany({
+					where: { status: 'activate' },
+					skip,
+					take,
+				}),
+				this.prisma.class.count({ where: { status: 'activate' } }),
+			]);
+			return [classes, totalRecords];
+		} catch (err) {
+			throw err;
+		}
+	}
+}

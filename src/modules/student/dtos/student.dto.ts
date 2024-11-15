d3736d353035e7e6b029628b03e93a43a09a0db5
TMNhat1810 <minhnhat912002@gmail.com>
2024-11-15 17:51:31 +0700

    feat: add program id in student data

---
+++ src/modules/student/dtos/student.dto.ts
@@ -0,0 +1,29 @@
+import { User } from '@prisma/client';
+
+export class StudentDto {
+	constructor(user: User) {
+		this.id_user = user.id_user;
+		this.username = user.username;
+		this.name = user.name;
+		this.email = user.email;
+		this.gender = user.gender;
+		this.date_of_birth = user.date_of_birth;
+		this.phone_number = user.phone_number;
+		this.address = user.address;
+		this.create_at = user.create_at;
+		this.id_role = user.id_role;
+		this.id_program = user.id_program;
+	}
+
+	id_user: string;
+	username: string;
+	name: string;
+	email: string;
+	gender: boolean;
+	date_of_birth: Date;
+	phone_number: string;
+	address: string;
+	create_at: Date;
+	id_role: string;
+	id_program: string;
+}

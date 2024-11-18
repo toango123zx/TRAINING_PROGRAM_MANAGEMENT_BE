c3fcfdaf8f409615b4bda2cd8417fbb7c67503c4
TMNhat1810 <minhnhat912002@gmail.com>
2024-11-18 18:56:08 +0700

    feat: add api get classes by student id

--- src/modules/student/queries/handlers/index.ts
+++ src/modules/student/queries/handlers/index.ts
@@ -1,4 +1,9 @@
 import { GetAllStudentHandler } from './get-all-student.handler';
+import { GetClassesByStudentIdHandler } from './get-classes-by-student-id.handler';
 import { GetStudentByIdHandler } from './get-student-by-id.handler';
 
-export const StudentQueryHandler = [GetAllStudentHandler, GetStudentByIdHandler];
+export const StudentQueryHandler = [
+	GetAllStudentHandler,
+	GetStudentByIdHandler,
+	GetClassesByStudentIdHandler,
+];
--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Request, UseGuards } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { PaginationDto } from 'src/common/dtos';
-import { GetAllStudentQuery, GetStudentByIdQuery } from './queries/implements';
+import {
+	GetAllStudentQuery,
+	GetClassesByStudentIdQuery,
+	GetStudentByIdQuery,
+} from './queries/implements';
+import { AuthGuard, RoleGuard } from 'src/shared/guards';
+import { Authorize } from 'src/common/decorators';
+import { Role } from 'src/common/enums';
 
 @ApiTags('Student')
 @Controller('student')
@@ -12,6 +19,16 @@ export class StudentController {
 	@Get()
 	async getUsers(@Query() pagination: PaginationDto) {
 		return this.queryBus.execute(new GetAllStudentQuery(pagination));
+	}
+
+	@Get('/current/class')
+	@ApiBearerAuth()
+	@UseGuards(AuthGuard, RoleGuard)
+	@Authorize(Role.Admin, Role.Student)
+	async getCurrentStudentClasses(@Request() request: any) {
+		return this.queryBus.execute(
+			new GetClassesByStudentIdQuery(request.user.id_user),
+		);
 	}
 
 	@Get('/:id')

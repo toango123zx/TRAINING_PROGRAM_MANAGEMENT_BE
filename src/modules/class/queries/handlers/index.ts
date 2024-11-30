32d3c4560cfaa03538e8b765207fdb65d85c80ac
TMNhat1810 <minhnhat912002@gmail.com>
2024-11-30 10:10:27 +0700

    feat: add api get students by class id

--- src/modules/class/queries/handlers/index.ts
+++ src/modules/class/queries/handlers/index.ts
@@ -1,3 +1,4 @@
 import { GetAllClassHandler } from './get-all-class.handler';
+import { GetStudentsByClassIdHandler } from './get-students-by-class-id.handler';
 
-export const QueryHandlers = [GetAllClassHandler];
+export const QueryHandlers = [GetAllClassHandler, GetStudentsByClassIdHandler];
